0021c2ead8920435b43ef2a40787e89bcb20b47c
Rakshit879 <grakshit430@gmail.com>
2025-08-04 14:01:15 +0530

    Image updated

--- components/sections/FeaturedProducts.tsx
+++ components/sections/FeaturedProducts.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 const products = [
   {
     name: "Stainless Steel Electrode",
-    image: "/images/MRF.png",
+    image: "/images/WELD BOND IMAGE.jpg",
   },
   {
     name: "Mild Steel Electrode",
@@ -13,7 +13,7 @@ const products = [
   },
   {
     name: "Cutting Electrode",
-    image: "/images/MRF.png",
+    image: "/images/WELD BOND IMAGE.jpg",
   },
 ];
 
